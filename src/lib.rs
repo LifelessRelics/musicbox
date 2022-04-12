2c2731d201e45ad1f41c6b0419fb5ddabfbc409f
holozetta@hologramhall.io <Ziebko6@gmail.com>
2022-04-12 11:43:17 -0700

    exposed basic building blocks phi, lambda, and twlv_root_two to lib.rs and called this patch 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,33 @@
 /// Feeding a music AI
 /// More bold would be to create a VST
 /// All contributions welcome, love to see how it plays out.
-
-
 mod fill_harmonics;
 pub mod natural_music_box;
 pub mod western_music_box;
-
 
 /// Adjusts quantity of notes in hashmap
 pub const SCOPE: i64 = 365; 
 /// Music box tuning
 pub const TONIC: f64 = 432.0;
+
+///Foundation for western music theory half-step
+pub fn phi() -> f64{
+   ( 1.0 + 5.0_f64.sqrt() ) / 2.0
+}
+
+pub fn twlv_root_two() -> f64 {
+    2.0_f64.powf(1.0/12.0)
+}
+
+/// This function takes in a tonic frequency and returns the frequency corresponding to the math equation Interval_frequency(degree) = TONIC* 2.0^(degree/12)
+pub fn lambda(tonic_freq: u64, degree:i64) -> f64 {
+    let interval = tonic_freq as f64 * 2.0_f64.powf((degree/ 12) as f64);
+    interval
+}
+
+
+
+
 
 #[test]
 ///Ensure the indexing is correct and an entry exists
